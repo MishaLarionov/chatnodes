82739790eeed63d39a13c6882a86de59eda5ad30
Misha Larionov <MishaLarionov@users.noreply.github.com>
2019-02-23 01:47:25 -0500

    Made something happen I remember everything

--- app/src/main/java/com/mishalarionov/chatnodes/MainActivity.java
+++ app/src/main/java/com/mishalarionov/chatnodes/MainActivity.java
@@ -2,12 +2,34 @@ package com.mishalarionov.chatnodes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button testButton;
+    private TextView textBoi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testButton = findViewById(R.id.test_button);
+        textBoi = findViewById(R.id.textBoi);
+
+        testButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.test_button:
+                textBoi.setText(Double.toString(Math.random()));
+        }
     }
 }
