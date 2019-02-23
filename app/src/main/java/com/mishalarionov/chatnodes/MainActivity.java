ea184b6324826ef27c61c18031f2522a996e6322
Misha Larionov <MishaLarionov@users.noreply.github.com>
2019-02-23 02:10:24 -0500

    Enable Bluetooth

--- app/src/main/java/com/mishalarionov/chatnodes/MainActivity.java
+++ app/src/main/java/com/mishalarionov/chatnodes/MainActivity.java
@@ -1,28 +1,56 @@
 package com.mishalarionov.chatnodes;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button testButton;
     private TextView textBoi;
+    private BluetoothAdapter bluetoothAdapter;
+    final int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        testButton = findViewById(R.id.test_button);
+        Button testButton = findViewById(R.id.test_button);
         textBoi = findViewById(R.id.textBoi);
 
         testButton.setOnClickListener(this);
 
+        //Initialize the bluetooth adapter
+        final BluetoothManager bluetoothManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        assert bluetoothManager != null;
+        bluetoothAdapter = bluetoothManager.getAdapter();
+
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+
+        }
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == -1) {
+                Toast.makeText(getApplicationContext(), R.string.bluetooth_enable_success, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), R.string.bluetooth_enable_fail, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
